4398a49cfb20402516b8834d54df59230d9e76b2
Nic <nic@Nics-iMac.local>
2018-06-23 22:03:42 -0700

    updating proper syntax to use config vars

--- arsenalGoalBot.py
+++ arsenalGoalBot.py
@@ -16,7 +16,7 @@ def authenticate():
     if is_prod:
         r = praw.Reddit(username = os.environ['username'],
                 password = os.environ['password'],
-                client_id = cos.environ['client_id'],
+                client_id = os.environ['client_id'],
                 client_secret = os.environ['client_secret'],
                 user_agent = "Arsenal Goal Bot v0.1",
                 )
